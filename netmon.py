718628f7a7152636d1968e25df5e5d14ccf667e8
Melissa Rodriguez <olenellina@gmail.com>
2017-01-02 15:09:35 -0800

    cleaning up comments, as I work through requirements

--- netmon.py
+++ netmon.py
@@ -9,17 +9,9 @@ import os
 # Inbound port 80 (OrPort)
 # Inbound port 443 (DirPort)
 # General ping (so that if first two tests fail, I will no if the server is at least responsive)
-# Idea here is that, if one of these tests fail --> push notification generated to mobile app
-
-# Might still want a VERY basic cron job that tests for the Tor PID running and sends to API if not (YES)
-# Might want to get dynamic DNS or something to handle comcast changing WAN IP
-# Script should allow people to define their own ports, so those should not be configured (instead passed by cron)
 
 def tcp_test(server_info):
-    # Really, what I want to do here is test specific ports associated with tor
-    # I could have functions that test inbound over Tor ports
-
-    # This DOES test connectivity over a specified portg
+    # This code will be updated to have tests for specific ports
     cpos = server_info.find(':')
     try:
         sock = socket()
@@ -30,34 +22,15 @@ def tcp_test(server_info):
     except:
         return False
 
-# Not sure this test is needed or what I would use it for:
-# def http_test(server_info):
-#     # The tor server is not a web server, so I don't think I need this function
-#     # Maybe have a generic ping test here
-#     try:
-#         data = urlopen(server_info).read()
-#         return True
-#     except:
-#         return False
-
 def ping_test(server_info):
-    hostname = "google.com" #example
-
+    # This code will handle the basic ping test
+    # Needs to be updated to strip away the port that is passed in currently
     response = os.system("ping -c 1 " + server_info)
 
-    #and then check the response...
     if response == 0:
       print(server_info, 'is up!')
     else:
       print(server_info, 'is down!')
-
-    # response = os.system("ping -c 1 " + hostname)
-    #
-    # #and then check the response...
-    # if response == 0:
-    #   print(hostname, 'is up!')
-    # else:
-    #   print(hostname, 'is down!')
 
 def server_test(server_info):
     # I guess this is the main function that executes others?
@@ -71,3 +44,19 @@ if __name__ == '__main__':
         print('Wrong number of arguments.')
     elif not server_test(argv[1]):
         print('Unable to connect to the service.')
+
+# General Notes:
+# Idea here is that, if one of these tests fail --> push notification generated to mobile app
+# There will be another script on the tor relay itself to monitor the Tor PID and send results to API
+# Might want to get dynamic DNS or something to handle comcast changing WAN IP
+# Script should allow people to define their own ports, so those should not be configured (instead passed by cron)
+
+# Code I No Longer Need:
+# def http_test(server_info):
+#     # The tor server is not a web server, so I don't think I need this function
+#     # Maybe have a generic ping test here
+#     try:
+#         data = urlopen(server_info).read()
+#         return True
+#     except:
+#         return False
