1fe40c8a743e3888631dbaa4929f336872d5121b
Melissa Rodriguez <olenellina@gmail.com>
2017-01-03 10:22:46 -0800

    updated pidmon to include timestamp

--- pidmon.py
+++ pidmon.py
@@ -1,7 +1,15 @@
 from sys import argv
 import psutil
+from datetime import datetime
+
+# So I think what I want to do here, is make a POST to the app engine backend with status & timestamp
+# I want this script to be called by cron every minute
+
+current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 if psutil.pid_exists(int(argv[1])):
     print("a process with pid %d exists" % int(argv[1]))
+    print("timestamp %s" % current_time)
 else:
     print("a process with pid %d does not exist" % int(argv[1]))
+    print("timestamp %s" % current_time)
