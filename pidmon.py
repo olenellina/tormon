3232c63f087dc2ed3c9936d15bdffff84f3c9342
Melissa Rodriguez <olenellina@gmail.com>
2017-01-03 20:44:22 -0800

    reworking pidmon to handle on server monitoring functions

--- pidmon.py
+++ pidmon.py
@@ -7,11 +7,26 @@ from datetime import datetime
 # So I think what I want to do here, is make a POST to the app engine backend with status & timestamp
 # I want this script to be called by cron every minute
 
-current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+def pid_test(pid):
 
-if psutil.pid_exists(int(argv[1])):
-    print("a process with pid %d exists" % int(argv[1]))
-    print("timestamp %s" % current_time)
-else:
-    print("a process with pid %d does not exist" % int(argv[1]))
-    print("timestamp %s" % current_time)
+    if psutil.pid_exists(int(pid)):
+        return True
+    else:
+        return False
+
+def net_test():
+    result = psutil.net_io_counters()
+    print(result)
+
+if __name__ == '__main__':
+    pid = argv[1]
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    if not pid_test(pid):
+        print("a process with pid %d exists" % int(pid))
+        print("timestamp %s" % current_time)
+    else:
+        print("a process with pid %d does not exist" % int(pid))
+        print("timestamp %s" % current_time)
+
+    net_test()
