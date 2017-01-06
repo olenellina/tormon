6238f5f948b9d4a6fd4442ef384b871a4d390351
Melissa Rodriguez <olenellina@gmail.com>
2017-01-06 13:35:26 -0800

    ConfigParser working correctly

--- pidmon.py
+++ pidmon.py
@@ -6,27 +6,47 @@ from datetime import datetime
 from stem.util.system import pid_by_name
 import sys
 from stem.util.connection import get_connections, system_resolvers
+import ConfigParser
+
+# Configuring secure storage of API key:
+Config = ConfigParser.ConfigParser()
+Config.read("config.ini")
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+api_key = ConfigSectionMap("SectionOne")['api_key']
+print api_key
 
 # Starting integration of PyFCM here:
 # Send to single device.
-from pyfcm import FCMNotification
-
-push_service = FCMNotification(api_key="<api-key>")
-
-# Your api-key can be gotten from:  https://console.firebase.google.com/project/<project-name>/settings/cloudmessaging
-
-registration_id = "<device registration_id>"
-message_title = "Uber update"
-message_body = "Hi john, your customized news for today is ready"
-result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
-
-# Send to multiple devices by passing a list of ids.
-registration_ids = ["<device registration_id 1>", "<device registration_id 2>", ...]
-message_title = "Uber update"
-message_body = "Hope you're having fun this weekend, don't forget to check today's news"
-result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
-
-print result
+# from pyfcm import FCMNotification
+#
+# push_service = FCMNotification(api_key="<api-key>")
+#
+# # Your api-key can be gotten from:  https://console.firebase.google.com/project/<project-name>/settings/cloudmessaging
+#
+# registration_id = "<device registration_id>"
+# message_title = "Uber update"
+# message_body = "Hi john, your customized news for today is ready"
+# result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
+#
+# # Send to multiple devices by passing a list of ids.
+# registration_ids = ["<device registration_id 1>", "<device registration_id 2>", ...]
+# message_title = "Uber update"
+# message_body = "Hope you're having fun this weekend, don't forget to check today's news"
+# result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
+#
+# print result
 
 # End integration of PyFCM
 
