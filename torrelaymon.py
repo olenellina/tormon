ea5f1127dac8eaf8e232d2eb47deb013cee82c9c
Melissa Rodriguez <olenellina@gmail.com>
2017-01-12 14:20:46 -0800

    implemented building data hash from results of actual tests for posting to app engine (no longer using stub data)

--- torrelaymon.py
+++ torrelaymon.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python
 
-# Begin conversion for integration with app engine
-
 # Needs to answer four questions:
 # Is the Tor pid (or pids) running? (complete)
 # What are the network stats? (complete)
@@ -9,10 +7,9 @@
 # What are my relays flags (am I a guard?)?
 
 import psutil
-from datetime import datetime
 import stem.control
 from stem.util.system import pid_by_name
-import sys
+# import sys
 # from stem.util.connection import get_connections, system_resolvers
 import requests
 
@@ -23,13 +20,14 @@ def pid_test():
 
     if not tor_pids:
         print("Unable to get tor's pid. Is it running?")
-        appengine_send(False)
-
-        sys.exit(1)
+        data['tor_pid'] = False
     elif len(tor_pids) > 1:
         print("You're running %i instances of tor, picking the one with pid %i" % (len(tor_pids), tor_pids[0]))
+        data['tor_pid'] = True
     else:
         print("Tor is running with pid %i" % tor_pids[0])
+        data['tor_pid'] = True
+
 
 # Network Traffic Test
 def net_io_test():
@@ -40,13 +38,14 @@ def net_io_test():
 # Tor Secific Network Test
 def tor_net_test():
     # Stub pid, until Tor is running and integrated
+    tor_net_connections = 0
     tor_pids.append(1056)
 
     all_traffic = psutil.net_connections(kind='tcp')
     for traffic in all_traffic:
         if traffic.pid in tor_pids:
-            print(traffic)
-    print(len(traffic))
+            tor_net_connections += 1
+    data['net_connections'] = tor_net_connections
 
 # Tor Flag Test:
 def flag_test():
@@ -54,26 +53,20 @@ def flag_test():
     print(user_traffic)
 
 # Function that will handle packaging and sending of data to app engine:
-def appengine_send(tor_pid):
-    # r = requests.get("http://torrelaymonitoring.appspot.com/?q=stotle")]
-    if tor_pid == False:
-        r = requests.post("http://torrelaymonitoring.appspot.com/?name=stotle&tor_pid=False")
-
+def appengine_send():
+    r = requests.post("http://torrelaymonitoring.appspot.com", data=data)
     print(r.status_code)
     print(r.content)
 
-
 if __name__ == '__main__':
-    # Perhaps run all of the tests which builds the payload and then call appengine_send at the bottom of this list
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     tor_pids = []
-    payload = {}
+    data = {'name': 'Thuum', 'tor_pid': None, 'net_connections': None}
 
     net_io_test()
     tor_net_test()
     flag_test()
-    # appengine_send()
     pid_test()
+    appengine_send()
 
 ######## BEGIN POST INTEGRATION:
 # https://cloud.google.com/appengine/docs/python/issue-requests
