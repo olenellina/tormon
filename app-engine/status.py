110e3c0abfec02750ec01940a32159861969b9c9
Melissa Rodriguez <olenellina@gmail.com>
2017-01-19 21:19:09 -0800

    cron script for monitoring last heartbeat status from app engine

---
+++ app-engine/status.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+import webapp2
+from datetime import datetime
+from google.appengine.ext import ndb
+import app
+
+class MainPageHandler(webapp2.RequestHandler):
+    def get(self):
+        query = app.Heartbeats.query()
+        last = query.order(-app.Heartbeats.last_check_in).get();
+        current_time = datetime.now()
+        diff = current_time - last.last_check_in
+        min_diff = diff.total_seconds() / 60
+        # if min_diff > 3:
+        #     app.fcm_send("Tor server unresponsive")
+        self.response.out.write(min_diff)
+
+status = webapp2.WSGIApplication([
+    webapp2.Route(r'/status', handler=MainPageHandler, name='status'),
+    ],
+    debug=True)
+
+# dev_appserver.py ./ --port=8080 --admin_port=8000
