6f535671d4346ec3afd05b1b57bd704a2e3337f3
Melissa Rodriguez <olenellina@gmail.com>
2017-01-23 10:44:46 -0800

    refactored code to have each error type have its own database property to allow one notification when an issue occurs but only one if the each is continuous

--- app-engine/status.py
+++ app-engine/status.py
@@ -12,8 +12,10 @@ class MainPageHandler(webapp2.RequestHandler):
         current_time = datetime.now()
         diff = current_time - last.last_check_in
         min_diff = diff.total_seconds() / 60
-        if min_diff > 3 and last.name == "Testing":
+        if min_diff > 3 and last.server_down == False and last.name == "Testing":
             app.fcm_send("Tor server unresponsive")
+            last.server_down = True
+            last.put()
         self.response.out.write(min_diff)
 
 status = webapp2.WSGIApplication([
